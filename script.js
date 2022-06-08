541e5f673c862043636e70f4e67d88a131037343
rita <maria.rita27012@gmail.com>
2022-06-08 09:43:00 -0300

    commit html

---
+++ script.js
@@ -0,0 +1,5 @@
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let sizePassowrd = document.querySelector("#valor");
+let password =
