3ae4692666aba159fe49c713d2c6a2d5763e913b
rita <maria.rita27012@gmail.com>
2022-06-08 10:11:21 -0300

    commit js

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
+
 let sizePassowrd = document.querySelector("#valor");
-let password =
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+let novaSenha = "";
+
+sizePassowrd.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function() {
+    sizePassowrd.innerHTML = this.value;
+}
+
+function generatePassword() {
+
+    let pass="";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+
+}
+
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!");
+    navigator.clickboar.writeText(novaSenha);
+}
